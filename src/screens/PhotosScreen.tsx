4d17587f2e6a2b631907d05098141ff4e543c301
Samip Bikram Thapa <samipthapa99@gmail.com>
2023-06-17 14:10:06 +0545

    Image grid working

--- src/screens/PhotosScreen.tsx
+++ src/screens/PhotosScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image, FlatList } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import ImagePicker from 'react-native-image-crop-picker';
@@ -7,9 +7,37 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { setImagePath } from "../redux/actions";
 
+const formatData = (data, numColumns) => {
+    const numberOfFullRows = Math.floor(data.length / numColumns);
+
+    let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
+    while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
+        data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
+        numberOfElementsLastRow++;
+    }
+
+    return data;
+};
+
+const numColumns = 3;
+
+const renderItem = ({ item, index }) => {
+    if (item.empty === true) {
+        return <View style={[styles.item, styles.itemInvisible]} />;
+    }
+    return (
+        <View style={styles.item}>
+            <Image
+                source={{ uri: item }}
+                style={styles.imageStyle}
+            />
+        </View>
+    );
+};
+
 function PhotosScreen(): JSX.Element {
-    const [image, setImage] = React.useState<string[]>([]);
     const dispatch = useDispatch();
+    const paths = useSelector(state => state.imagePath);
 
     const takePhotoFromCamera = () => {
         ImagePicker.openCamera({
@@ -17,16 +45,11 @@ function PhotosScreen(): JSX.Element {
             height: 400,
             cropping: false,
         }).then(image => {
-            setImage(prev => [
-                ...prev,
-                image.path
-            ]);
             dispatch(setImagePath(image.path));
         });
     };
 
-    const paths = useSelector(state => state.imagePath);
-    console.log(paths);
+    const formattedData = formatData(paths, numColumns);
 
     return (
         <View style={styles.container}>
@@ -35,24 +58,14 @@ function PhotosScreen(): JSX.Element {
             <Text style={styles.textStyle}>Photo</Text>
             <Text style={styles.subheading}>Take photo of the place you set in advance</Text>
 
-            {/* <View style={{ flex: 1 }}>
-                {image &&
-                    <FlatList
-                        data={image}
-                        numColumns={2}
-                        renderItem={({ item }) => {
-                            return (
-                                <Image
-                                    source={{ uri: item }}
-                                    style={styles.imageStyle}
-                                />
-                            )
-                        }}
-                        keyExtractor={item => item}
-                        contentContainerStyle={{ flexGrow: 1 }}
-                    />
-                }
-            </View> */}
+            <View style={{ flex: 1 }}>
+                <FlatList
+                    data={formattedData}
+                    style={styles.imageContainerStyle}
+                    numColumns={3}
+                    renderItem={renderItem}
+                />
+            </View>
 
             <TouchableOpacity
                 style={styles.addPhoto}
@@ -104,7 +117,6 @@ const styles = StyleSheet.create({
         borderRadius: 7,
         paddingHorizontal: 25,
         paddingVertical: 20,
-        marginTop: '5%',
     },
     addPhotoText: {
         fontSize: 19,
@@ -125,12 +137,24 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         paddingVertical: 12,
         alignSelf: 'center',
-        marginTop: '130%'
     },
     imageStyle: {
-        width: 175,
-        height: 175,
-        borderRadius: 5,
+        height: 120,
+        width: '100%',
+        borderRadius: 5
+    },
+    imageContainerStyle: {
+        flex: 1,
+        marginVertical: 20,
+    },
+    item: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        margin: 2,
+    },
+    itemInvisible: {
+        backgroundColor: 'transparent',
     },
 });
 
